16f5856c34f24c86622cf5cf672e725fa2d84533
Gesiel Souza Oliveira <gesiel.br@gmail.com>
2021-05-04 10:15:42 -0300

    atualização banner e banner mobile inclusao aviso, atualização variável JS

--- q10l/js/main.js
+++ q10l/js/main.js
@@ -20,22 +20,22 @@ $(document).ready(function () {
 
 // CONTADOR
 $(document).ready(function () {
-  var timer2 = "20:00";
+  var timer1 = "20:00";
   var interval = setInterval(function () {
-    var timer = timer2.split(":");
+    var timer = timer1.split(":");
     var minutos = parseInt(timer[0], 10);
     var segundos = parseInt(timer[1], 10);
     --segundos;
     minutos = segundos < 0 ? --minutos : minutos;
     segundos = segundos < 0 ? 59 : segundos;
     segundos = segundos < 10 ? "0" + segundos : segundos;
-    $("#min").html(minutos);
-    $("#seg").html(segundos);
+    $(".min").html(minutos);
+    $(".seg").html(segundos);
     $(".countdown").html(minutos + ":" + segundos);
     if (minutos < 0) clearInterval(interval);
     //check if both minutos and segundos are 0
     if (segundos <= 0 && minutos <= 0) clearInterval(interval);
-    timer2 = minutos + ":" + segundos;
+    timer1 = minutos + ":" + segundos;
   }, 1000);
 });
 
